3cad6b5d0e7ce00096761d240b8145456e6f80a1
Yoshimasa Niwa <niw@niw.at>
2018-02-21 16:24:31 -0800

    Clean up git configurations.

--- shared/git.sh
+++ shared/git.sh
@@ -1,10 +1,8 @@
 if which git >/dev/null 2>&1; then
+  # See also `.gitconfig`
   alias d="git diff"
   alias dc="git diff --cached"
-  alias s="git status"
-  alias a="git add"
-  alias grf="git remote -v|cut -f1|sort|uniq|xargs -n 1 git fetch"
-  alias gb="git symbolic-ref HEAD|cut -d'/' -f3"
-  alias gpoh="git push origin HEAD"
   alias gpodh="git push origin :\$(git symbolic-ref HEAD|cut -d"/" -f3)"
+  alias gpoh="git push origin HEAD"
+  alias s="git status"
 fi
